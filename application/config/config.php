88130d922b39a455af7e01c633c7079850045f12
Sergey Kuzmin <kuzmin.serega@gmail.com>
2018-06-20 10:14:32 +0300

    added base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://peaceful-thicket-30794.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
